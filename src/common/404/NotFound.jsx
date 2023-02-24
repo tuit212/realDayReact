c808b3255059669a3fcb0e318c629caefc3db847
tuit212 <asadbekanvarov9808@example.com>
2023-02-24 17:57:23 +0500

    firist commit

---
+++ src/common/404/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NotFound() {
+  return (
+    <div style={{textAlign:"center" , fontSize:"30px" , padding:"30px"}}>
+      404
+    </div>
+  )
+}
+
+export default NotFound

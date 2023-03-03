ad1bf1f6e080bd552819b2c0c56cff131f9cbd75
tuit212 <asadbekanvarov9808@example.com>
2023-03-03 14:31:32 +0500

    push

--- src/Pages/MainGeberal/MainGeneral.jsx
+++ src/Pages/MainGeberal/MainGeneral.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 
 function MainGeneral() {
-<<<<<<< HEAD
     return (
         <div className='section-menejer' style={{paddingTop:'70px'}}>
             MainGeneral
@@ -9,12 +8,5 @@ function MainGeneral() {
     )
 }
 
-export default MainGeneral
-=======
-  return (
-    <div>MainGeneral</div>
-  )
-}
 
 export default MainGeneral
->>>>>>> ec72427bc86510897e532e6ae70d8e7b2b61ffe1

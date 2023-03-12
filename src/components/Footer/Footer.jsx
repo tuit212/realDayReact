84f93bd985df0d9ead78f6889ff7e1807ee63c88
tuit212 <asadbekanvarov9808@example.com>
2023-03-12 10:48:10 +0500

    footer add

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
+import './Footer.scss'
+import { Link } from 'react-router-dom'
+import logo from '../../Assets/images/logo.svg'
 
 function Footer() {
     return (
-        <div>
-            Footer
+        <div className='footer__section'>
+            <div className="footer__top">
+                <div className="container">
+                    <div className="footer">
+                        <ul className='list'>
+                            <li className="item">
+                                <Link to={"/"}>
+                                    LOGO
+                                </Link>
+                            </li>
+                            <li className="item">
+                                <Link to={"/register"}>
+                                    Registratsiyadan o’tish
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div className="footer__bottom">
+                <div className="container">
+                    <div className="footer-bottom">
+                        <p>2023-yil</p>
+                        <p>GROUP tomonidan Ishlab chiqilgan </p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- src/data/wedding.js
+++ src/data/wedding.js
@@ -32,6 +32,7 @@ export const posts = [
         text2 :"kishi boshiga" ,
         bottom3 : "Bor" ,
         text3 : "Qo‘shimcha bonus" ,
+        logo : `${logo}`,
         images : "https://avatars.mds.yandex.net/get-altay/1666174/2a0000016bf9093ac0efc8dde787079290fb/XXL_height",
         img1 : "https://avatars.mds.yandex.net/get-altay/3766813/2a00000176c922f3b09ac7f428f9c1fe6d9e/XXL_height" , 
         img3 : "https://strg1.restoran.kz/neofiles/serve-image/5581158c7265736ebe450100/1200x1200/q90" , 
